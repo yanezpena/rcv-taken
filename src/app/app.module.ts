720a4ab746d3d28b2c067977f07e846a69199f07
yanezpena <yanezpena@gmail.com>
2021-10-22 18:22:24 -0400

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { WebcamModule } from 'ngx-webcam';
+
+@NgModule({
+	declarations: [AppComponent],
+	imports: [BrowserModule, FormsModule, AppRoutingModule, WebcamModule],
+	providers: [],
+	bootstrap: [AppComponent],
+})
+export class AppModule {}
